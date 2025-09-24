8d25b8b1a1edb98716354ea2e4f531cf1c10b08f
JORDAN\cocdr <nutkoko675@gmail.com>
2025-09-24 17:04:54 +0800

    CHANGE

--- otp.php
+++ otp.php
@@ -167,23 +167,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </section>
     </main>
 
-    <footer class="py-12">
+    <<footer class="py-12">
         <div class="container">
             <div class="flex flex-wrap -mx-3">
                 <div class="flex-shrink-0 w-full max-w-full mx-auto mb-6 text-center lg:flex-0 lg:w-8/12">
-                    <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">Company</a>
                     <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">About Us</a>
-                    <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">Team</a>
-                    <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">Products</a>
-                    <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">Blog</a>
                     <a href="javascript:;" target="_blank" class="mb-2 mr-4 text-slate-400 sm:mb-0 xl:mr-12">Pricing</a>
                 </div>
                 <div class="flex-shrink-0 w-full max-w-full mx-auto mt-2 mb-6 text-center lg:flex-0 lg:w-8/12">
-                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400"><span class="text-lg fab fa-dribbble"></span></a>
-                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400"><span class="text-lg fab fa-twitter"></span></a>
-                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400"><span class="text-lg fab fa-instagram"></span></a>
-                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400"><span class="text-lg fab fa-pinterest"></span></a>
-                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400"><span class="text-lg fab fa-github"></span></a>
+                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+                        <span class="text-lg fab fa-dribbble"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+                        <span class="text-lg fab fa-twitter"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+                        <span class="text-lg fab fa-instagram"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+                        <span class="text-lg fab fa-pinterest"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="mr-6 text-slate-400">
+                        <span class="text-lg fab fa-github"></span>
+                    </a>
                 </div>
             </div>
             <div class="flex flex-wrap -mx-3">
@@ -193,8 +199,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <script>
                             document.write(new Date().getFullYear());
                         </script>
-                        Soft by Creative Tim.
-                        <span class="w-full"> Distributed by ❤️ ThemeWagon </span>
+                    
                     </p>
                 </div>
             </div>
